d021f5fbaf80c7b25c8dc09008e59c08d9d8c217
AdrianSole <adrimaella@gmail.com>
2023-08-02 12:50:41 +0200

    Modal

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -1,6 +1,9 @@
 import styled from "@emotion/styled";
 import { Pagination } from "../Pagination";
 import { useCharacterList } from "../CharacterContext";
+import Modal from "react-modal";
+import { useState } from "react";
+import { Character } from "src/api/types/Character";
 
 const ListContainer = styled("div")`
   display: flex;
@@ -23,20 +26,73 @@ const ListItem = styled("li")`
   position: relative;
   padding-left: 34px;
   margin: 10px;
+  &:hover {
+    cursor: pointer;
+    color: blue;
+  }
 `;
 
 export const CharacterList = () => {
   const context = useCharacterList();
 
+  //Modal
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalData, setModalData] = useState<Character>();
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
-      {console.log(context.paginationState)}
       <ListContainer>
         <List>
           {context.listState?.map((characters) => (
-            <ListItem key={characters.id}>{characters.name}</ListItem>
+            <ListItem
+              key={characters.id}
+              onClick={() => {
+                openModal();
+                setModalData(characters);
+              }}
+            >
+              {characters.name}
+            </ListItem>
           ))}
         </List>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example"
+        >
+          <h2>{modalData?.name}</h2>
+          <button onClick={closeModal}>close</button>
+          <div>
+            <img src={modalData?.image} alt="alt" />
+            <ol>
+              <li>{modalData?.status}</li>
+              <li>{modalData?.species}</li>
+              <li>{modalData?.type}</li>
+              <li>{modalData?.gender}</li>
+              <li>{modalData?.created}</li>
+            </ol>
+          </div>
+        </Modal>
         <Pagination onPrev={context.onPrev} onNext={context.onNext} />
       </ListContainer>
     </>
