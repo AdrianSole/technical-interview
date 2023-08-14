3425f5012e6bf1e293c60a2c469410e98f6f9402
AdrianSole <adrimaella@gmail.com>
2023-08-14 12:16:42 +0200

    CharacterSearch test coverage

--- src/components/CharacterSearch/CharacterSearch.ui-jest.spec.tsx
+++ src/components/CharacterSearch/CharacterSearch.ui-jest.spec.tsx
@@ -1,8 +1,8 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { CharacterSearch } from "./CharacterSearch";
-import mockRouter from 'next-router-mock';
+import mockRouter from "next-router-mock";
 
-jest.mock('next/router', () => jest.requireActual('next-router-mock'))
+jest.mock("next/router", () => jest.requireActual("next-router-mock"));
 
 describe("CharacterSearch", () => {
   it("should update suggestions", async () => {
@@ -27,5 +27,32 @@ describe("CharacterSearch", () => {
 
     expect(textBox).toBeInTheDocument();
     expect(suggestions).toBeInTheDocument();
+  });
+
+  it("should redirect to the dynamic page", () => {
+    mockRouter.push("/initial-path");
+    const { getByTestId } = render(<CharacterSearch />);
+    const textBox = getByTestId("textbox");
+
+    fireEvent.focus(textBox);
+    fireEvent.blur(textBox);
+  });
+
+  it("should show suggestions on input focus and hide on blur", async () => {
+    mockRouter.push("/initial-path");
+    render(<CharacterSearch />);
+
+    const searchInput = screen.getByPlaceholderText("Search a character...");
+
+    fireEvent.focus(searchInput);
+    await waitFor(() => {
+      expect(screen.getByTestId("suggestions")).toBeVisible();
+    });
+
+    fireEvent.blur(searchInput);
+
+    await waitFor(() => {
+      expect(screen.getByTestId("suggestions")).toBeVisible();
+    });
   });
 });
