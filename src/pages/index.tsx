b2e5b17af9b92aeb79e82cd92365af083c229368
AdrianSole <adrimaella@gmail.com>
2023-08-15 11:06:21 +0200

    getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import Head from "next/head";
 import { CharacterList } from "src/components/CharacterList";
 import { CharacterProvider } from "src/components/CharacterContext";
+import { GetServerSideProps } from "next";
+import { Character } from "src/api/types/Character";
+import { PaginationInfo } from "src/api/types/PaginationInfo";
+import * as characterService from "../api/services/CharacterService";
+
+
+interface CharacterListProps {
+  listState: Character[];
+  paginationState: PaginationInfo;
+}
 
 export default function Home() {
   return (
@@ -16,3 +26,39 @@ export default function Home() {
   );
 }
 
+export const getServerSideProps: GetServerSideProps<CharacterListProps> = async () => {
+  try {
+    const cache = localStorage.getItem('cacheList');
+    if (cache) {
+      const { results, info } = JSON.parse(cache);
+
+      return {
+        props: {
+          listState: results,
+          paginationState: info,
+        },
+      };
+    } else {
+      const res = await characterService.getCharacters();
+      const { results, info } = res.data;
+
+      localStorage.setItem('cacheList', JSON.stringify(res.data));
+
+      return {
+        props: {
+          listState: results,
+          paginationState: info,
+        },
+      };
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+
+    return {
+      props: {
+        listState: [],
+        paginationState: {},
+      },
+    };
+  }
+};
